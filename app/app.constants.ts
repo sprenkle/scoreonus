7f381fa1bd5a71a40a111f2ee4d4ac03719af18e
sprenkle <david@sprenkle.net>
2016-12-20 22:13:38 -0700

    Checked into site

--- app/app.constants.ts
+++ app/app.constants.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-//    public Server: string = "http://scoreon.us:8080/";
-    public Server: string = "http://192.168.1.31:8080/";
+    public Server: string = "http://scoreon.us:8080/scoreoneusrest/";
+//    public Server: string = "http://scoreon.us:8080/scoreonus";
     public ApiUrl: string = "api/";
     public ServerWithApiUrl = this.Server + this.ApiUrl;
 }
